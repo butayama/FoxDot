72bf11eb12118dc4bdb4f11cf0da8984e72fce0c
Uwe Schweinsberg <uwe.schweinsberg@t-online.de>
2021-06-09 13:17:33 +0200

    # include files:

--- own_files/create_reggae_music.py
+++ own_files/create_reggae_music.py
@@ -4,6 +4,9 @@
 # http://studio.dubroom.org/tutorials-computerdub07.htm
 # THE NYABINGHI RHYTHM
 
+# include files:
+# https://stackoverflow.com/questions/714881/how-to-include-external-python-code-to-use-in-other-files
+
 
 def csr(bpm=140, scale="chromatic",root="C"):
     Clock.bpm = bpm
